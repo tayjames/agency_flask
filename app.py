bbc2afa0f9a04ddd6f31f40a040d266ec762d8f7
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-10-29 15:06:28 -0600

    Changes heroku to flask_heroku

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from datetime import datetime
 from errors import bad_request
-from heroku import Heroku
+from flask_heroku import Heroku
 import os
 import bcrypt
 import logging
